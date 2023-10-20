101288a24c51dd4e86dbf68f82d61fb6aa314597
eduardomelonunes2001@gmail.com <eduardomelonunes2001@gmail.com>
2023-10-20 19:18:35 -0300

    Glória

---
+++ cadastro-questionador/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import SignUp from './SignUp';
+
+function App() {
+    return (
+        <div className="App">
+            <SignUp />
+        </div>
+    );
+}
+
+export default App;
