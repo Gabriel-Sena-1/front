1aaa893c892aac10dfdea63652cc808b79f9d05c
wendeson amaro de souza <wenderson3souza@gmail.com>
2024-09-24 00:50:22 -0300

    fix style application

--- tira_duvidas/src/layouts/root-unauthenticated-layout.tsx
+++ tira_duvidas/src/layouts/root-unauthenticated-layout.tsx
@@ -8,11 +8,13 @@ export function RootUnauthenticatedLayout() {
   return (
     <Flex
       as="main"
+      direction="column"
       minWidth="500px"
-      className="min-h-screen min-w-screen text-zind-900 bg-gradient-to-t from-blue-600 to-violet-600"
+      style={{ backgroundColor: "#3498DB" }}
     >
+      <Flex w="full" height={81} className="bg-white"></Flex>
       <Flex w="full" flex={1} className="min-w-screen min-h-screen">
-        <Flex w="full" flex={1} py={8} px={8} className="w-full min-w-screen">
+        <Flex w="full" flex={1} className="w-full min-w-screen">
           {state === "loading" ? (
             <>
               <LoadingLogo />
--- tira_duvidas/src/screens/FAQ/FAQSection.tsx
+++ tira_duvidas/src/screens/FAQ/FAQSection.tsx
@@ -1,50 +1,71 @@
-import React from 'react';
-import styles from './FAQSection.module.css';
-import FAQItem from './FAQItem';
+import React from "react";
+import styles from "./FAQSection.module.css";
+import FAQItem from "./FAQItem";
+import "./FAQSection.module.css";
+import { Flex } from "@chakra-ui/layout";
 
-const categories = ['Categoria1', 'Categoria2', 'Categoria3', 'Categoria4', 'Categoria5'];
+const categories = [
+  "Categoria1",
+  "Categoria2",
+  "Categoria3",
+  "Categoria4",
+  "Categoria5",
+];
 
 const faqItems = [
   {
-    title: 'Título da Pergunta 1',
-    description: 'Descrição da pergunta bem detalhada com muitos detalhes mesmo, que vai ficar ao lado da imagem padrão ou a anexada pelo usuário',
-    imageSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08'
+    title: "Título da Pergunta 1",
+    description:
+      "Descrição da pergunta bem detalhada com muitos detalhes mesmo, que vai ficar ao lado da imagem padrão ou a anexada pelo usuário",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08",
   },
   {
-    title: 'Título da Pergunta 2',
-    description: 'Outra descrição detalhada de uma pergunta frequente.',
-    imageSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08'
+    title: "Título da Pergunta 2",
+    description: "Outra descrição detalhada de uma pergunta frequente.",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08",
   },
   {
-    title: 'Título da Pergunta 3',
-    description: 'Mais uma pergunta com uma explicação extensa para os usuários.',
-    imageSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08'
+    title: "Título da Pergunta 3",
+    description:
+      "Mais uma pergunta com uma explicação extensa para os usuários.",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08",
   },
   {
-    title: 'Título da Pergunta 4',
-    description: 'Pergunta quatro, que deve responder algo importante.',
-    imageSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08'
+    title: "Título da Pergunta 4",
+    description: "Pergunta quatro, que deve responder algo importante.",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08",
   },
   {
-    title: 'Título da Pergunta 5',
-    description: 'Descrição da quinta pergunta, com mais detalhes técnicos.',
-    imageSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08'
+    title: "Título da Pergunta 5",
+    description: "Descrição da quinta pergunta, com mais detalhes técnicos.",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08",
   },
   {
-    title: 'Título da Pergunta 6',
-    description: 'Sexta pergunta, ainda mais informações relevantes para o usuário.',
-    imageSrc: 'https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08'
-  }
+    title: "Título da Pergunta 6",
+    description:
+      "Sexta pergunta, ainda mais informações relevantes para o usuário.",
+    imageSrc:
+      "https://cdn.builder.io/api/v1/image/assets/TEMP/97b5ec677b6e996709eb5071b86bb54d347ed5b6ce6d9a506ea19b193938df05?placeholderIfAbsent=true&apiKey=d8d70c15437f46d9988865cdfeffad08",
+  },
 ];
 
 const FAQSection: React.FC = () => {
   return (
-    <section className={styles.faqContainer}>
+    <Flex className={styles.faqContainer}>
       {/* Adiciona a barra de categorias */}
       <header className={styles.categoryBar}>
         <nav className={styles.categoryList}>
           {categories.map((category, index) => (
-            <a key={index} href={`#${category.toLowerCase()}`} className={styles.categoryItem}>
+            <a
+              key={index}
+              href={`#${category.toLowerCase()}`}
+              className={styles.categoryItem}
+            >
               {category}
             </a>
           ))}
@@ -62,7 +83,7 @@ const FAQSection: React.FC = () => {
           </div>
         ))}
       </div>
-    </section>
+    </Flex>
   );
 };
 
